e33b76e844e85fb433f47d1337f168f238fa549b
JustK0na <mstruzykowski@gmail.com>
2023-05-08 01:21:52 +0200

    reszta klas

---
+++ src/Draw.h
@@ -0,0 +1,14 @@
+//
+// Created by Admin on 08.05.2023.
+//
+
+#ifndef SNAKE_DRAW_H
+#define SNAKE_DRAW_H
+
+
+class Draw {
+
+};
+
+
+#endif //SNAKE_DRAW_H
---
+++ src/Snake.h
@@ -0,0 +1,14 @@
+//
+// Created by Admin on 08.05.2023.
+//
+
+#ifndef SNAKE_SNAKE_H
+#define SNAKE_SNAKE_H
+
+
+class Snake {
+
+};
+
+
+#endif //SNAKE_SNAKE_H
